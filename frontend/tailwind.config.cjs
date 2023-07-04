632f767ab10820b94a6a574da907069acf9bf4d7
datnguyen-nfq <dat.nguyen@nfq.com>
2023-07-04 22:02:34 +0700

    Update tailwind config

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -4,6 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
