9e2037057b41da78139650a059314b8a309308a8
datnguyen-nfq <dat.nguyen@nfq.com>
2023-07-04 22:12:14 +0700

    Add config astro file with tailwind

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,7 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({ 
+    applyBaseStyles: false,
+  })],
   site: 'https://vymktg.com.github.io',
   base: '/',
 });
