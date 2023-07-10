9ed25455900123073f6e5a86a5c0a48e4bb18565
datnguyen-nfq <dat.nguyen@nfq.com>
2023-07-10 21:33:00 +0700

    Add animation typing

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -17,6 +17,31 @@ module.exports = {
           color: "rgb(23, 37, 84)",
         },
       },
+      keyframes: {
+        typing: {
+          '0%': {
+            width: '0%',
+            visibility: 'hidden',
+          },
+          '100%': {
+            width: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            borderColor: 'transparent',
+          },
+          '66%': {
+            borderColor: '#4943a5',
+          },
+        },
+      },
+      animation: {
+        typing: 'typing 3s steps(20) alternate, blink 1.2s infinite',
+      },
+      bottom: {
+        20: '5rem'
+      }
     },
   },
   plugins: [],
