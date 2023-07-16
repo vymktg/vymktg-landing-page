fd85a7e17d73f6b330a6c74debf3a8a4e2d7d581
datnguyen-nfq <dat.nguyen@nfq.com>
2023-07-16 17:18:55 +0700

    Restructure component

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -21,6 +21,49 @@ export const NAVIGATION_ITEM = [
   },
 ];
 
+export const SERVICE_LIST = [
+  {
+    title: 'Content Marketing',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    title: 'Digital Marketing',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  }
+  , {
+    title: 'Offline Marketing',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  }
+  , {
+    title: 'Video Marketing',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    title: 'Customer Relationship Management (CRM)',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  }
+  , {
+    title: 'Reputation Management',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  }
+];
+
+export const BY_NUMBERS = [
+  {
+    number: '12+',
+    title: 'Clients'
+  }, {
+    number: '8+',
+    title: 'Partners'
+  }, {
+    number: '20+',
+    title: 'Projects'
+  }, {
+    number: '5+',
+    title: 'Years'
+  },
+]
+
 export const HOW_IT_WORKS_STEP = [
   {
     title: "Initial Meeting",
@@ -57,6 +100,6 @@ export const SITE = {
   authorImage: '', // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
   // Author: Bio
   authorBio: 'Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro'
-  
+
 }
 
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
-  content: [
-    './public/**/*.html',
-    './src/**/*.{astro,js,ts}'
-  ],
+  content: ["./public/**/*.html", "./src/**/*.{astro,js,ts}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Raleway", ...defaultTheme.fontFamily.sans],
+      screens: {
+        midmd: "880px",
       },
       textHeading: {
         1: {
@@ -19,29 +14,29 @@ module.exports = {
       },
       keyframes: {
         typing: {
-          '0%': {
-            width: '0%',
-            visibility: 'hidden',
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
           },
-          '100%': {
-            width: '100%',
+          "100%": {
+            width: "100%",
           },
         },
         blink: {
-          '0%': {
-            borderColor: 'transparent',
+          "0%": {
+            borderColor: "transparent",
           },
-          '66%': {
-            borderColor: '#4943a5',
+          "66%": {
+            borderColor: "#4943a5",
           },
         },
       },
       animation: {
-        typing: 'typing 3s steps(20) alternate, blink 1.2s infinite',
+        typing: "typing 3s steps(20) alternate, blink 1.2s infinite",
       },
       bottom: {
-        24: '6rem'
-      }
+        24: "6rem",
+      },
     },
   },
   plugins: [],
