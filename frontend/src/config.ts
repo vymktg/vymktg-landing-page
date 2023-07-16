840d70e0de0e42c3fc5650f0395230f756b3af54
datnguyen-nfq <dat.nguyen@nfq.com>
2023-07-16 19:38:41 +0700

    Add FAQ section

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -23,28 +23,28 @@ export const NAVIGATION_ITEM = [
 
 export const SERVICE_LIST = [
   {
-    title: 'Content Marketing',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'Social Media Management',
+    description: 'Managing social media accounts effectively can be time-consuming and overwhelming.',
   },
   {
-    title: 'Digital Marketing',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'Website and SEO',
+    description: `Maximize your website's potential with my website management and optimization services which include updates content, performance optimization`,
   }
   , {
-    title: 'Offline Marketing',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'Online Reputation Management',
+    description: 'Maintaining a positive online reputation is essential for your business.',
   }
   , {
-    title: 'Video Marketing',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'Email & SMS Marketing',
+    description: 'Both email and text marketing are valuable tools for businesses to connect with their target audience, deliver relevant and personalized content, and drive engagement and conversions. ',
   },
   {
-    title: 'Customer Relationship Management (CRM)',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'Paid Advertising Campaigns',
+    description: 'Paid advertising campaigns can be an effective way for small businesses to accelerate their growth.',
   }
   , {
-    title: 'Reputation Management',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title: 'Content Creation',
+    description: 'Engaging and informative content plays a vital role in attracting and retaining customers.',
   }
 ];
 
@@ -62,6 +62,28 @@ export const BY_NUMBERS = [
     number: '5+',
     title: 'Years'
   },
+]
+
+export const FAQ_LIST = [
+  {
+    question: 'Can you work within my limited marketing budget?',
+    answer: 'Absolutely! Working with small budgets is a common challenge, and I specialize in helping businesses make the most of their resources. I can identify cost-effective tactics, prioritize activities that provide the highest impact, and leverage affordable digital platforms to optimize your marketing within your financial constraints.'
+  },
+  {
+    question: 'How can hiring you be cost-effective for my small business?',
+    answer: 'Hiring a marketing specialist like me can be a cost-effective solution for your small business. By leveraging my expertise and industry knowledge, I can help you make informed decisions, avoid costly mistakes, and allocate your budget wisely. This leads to optimized marketing strategies and a higher return on investment (ROI) for your marketing efforts.'
+  },
+  {
+    question: 'How do I know if my small business needs the assistance of a marketing consultant?',
+    answer: `If you're struggling to achieve your marketing goals, lack a clear strategy, or need fresh insights and ideas, it's a good indication that your business could benefit from my assistance as a marketing consultant. I can assess your current efforts, identify areas for improvement, and develop a customized plan to drive growth and increase brand visibility.`
+  },
+  {
+    question: 'How long does it usually take to see tangible results from marketing efforts?',
+    answer: 'The timeframe to see tangible results from marketing efforts can vary depending on several factors such as industry, target audience, and chosen marketing channels. Some strategies may yield immediate results, while others require more time to gain traction. During our collaboration, I will set realistic expectations and develop a timeline based on your objectives and business circumstances.'
+  }, {
+    question: 'Will you provide guidance on maximizing my marketing efforts while minimizing expenses?',
+    answer: 'Absolutely! One of my primary objectives as a marketing consultant is to help businesses optimize their marketing efforts within their budget limitations. I can provide strategies to maximize your ROI, identify cost-effective marketing channels, and suggest innovative approaches to achieve your marketing goals while minimizing expenses.'
+  }
 ]
 
 export const HOW_IT_WORKS_STEP = [
