e885a04d8926f5bb07a0327e0acae2dff42fba89
datnguyen-nfq <dat.nguyen@nfq.com>
2023-07-29 23:53:19 +0700

    Add breakpoint perpage

--- frontend/src/js/splide-init.ts
+++ frontend/src/js/splide-init.ts
@@ -1,11 +1,22 @@
 import Splide from "@splidejs/splide";
 
-const partnerSlider = new Splide(".splide", { 
-    type: 'loop',
-    perPage: 5,
-    arrows: false,
-    autoplay: true,
-    interval: 5000,
+const partnerSlider = new Splide(".splide", {
+  type: "loop",
+  perPage: 5,
+  breakpoints: {
+    1024: {
+      perPage: 4,
+    },
+    768: {
+      perPage: 3,
+    },
+    640: {
+      perPage: 2,
+    },
+  },
+  arrows: false,
+  autoplay: true,
+  interval: 5000,
 });
 
 partnerSlider.mount();
