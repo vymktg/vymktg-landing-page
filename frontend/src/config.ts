7a5b2413f6b22aa4ac030eab7be6abd068c407b3
datnguyen-nfq <dat.nguyen@nfq.com>
2023-07-29 23:15:08 +0700

    update services list

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -32,126 +32,160 @@ export const PARTNER_LIST = [
   // { url: "romanric.png" },
   // { url: "pate-and-butter.png" },
   // { url: "cp.png" },
-]
+];
 
 export const PROJECT_LIST = [
-  { url: "", name: "Project 1", image_url: "https://web.sociolib.com/centro/wp-content/uploads/sites/3/2023/01/31.jpg"},
-  { url: "", name: "Project 2", image_url: "https://web.sociolib.com/centro/wp-content/uploads/sites/3/2023/01/34.jpg"},
-  { url: "", name: "Project 3", image_url: "https://web.sociolib.com/centro/wp-content/uploads/sites/3/2023/01/28.jpg"},
-]
+  {
+    url: "",
+    name: "Project 1",
+    image_url:
+      "https://web.sociolib.com/centro/wp-content/uploads/sites/3/2023/01/31.jpg",
+  },
+  {
+    url: "",
+    name: "Project 2",
+    image_url:
+      "https://web.sociolib.com/centro/wp-content/uploads/sites/3/2023/01/34.jpg",
+  },
+  {
+    url: "",
+    name: "Project 3",
+    image_url:
+      "https://web.sociolib.com/centro/wp-content/uploads/sites/3/2023/01/28.jpg",
+  },
+];
 
 export const SERVICE_LIST = [
   {
-    title: 'Social Media Management',
-    description: 'Streamlined your social media accounts with engaging content',
-    icon: 'tabler:social'
+    title: "Social Media Management",
+    description: "Streamlined your social media accounts with engaging content",
+    icon: "tabler:social",
   },
   {
-    title: 'Website and SEO',
+    title: "Website and SEO",
     description: `Boost your online visibility with tailored SEO strategies and website management`,
-    icon: 'carbon:dns-services'
-  }
-  , {
-    title: 'Online Reputation Management',
-    description: 'Nurturing a positive reputation by building trust to drive sustainable business growth.',
-    icon: 'tabler:award',
-  }
-  , {
-    title: 'Online Reputation Management',
-    description: 'Nurturing a positive reputation by building trust to drive sustainable business growth',
-    icon: 'tabler:award',
+    icon: "carbon:dns-services",
   },
   {
-    title: 'Email & SMS Marketing',
-    description: 'Enhance customer engagement, nurture relationships with direct communication',
-    icon: 'mdi:email-seal-outline',
-  }
-  , {
-    title: 'Paid Advertising Campaigns',
-    description: 'Fuel your growth with effective paid advertising Campaigns',
-    icon: 'icons8:advertising'
-  }
+    title: "Online Reputation Management",
+    description:
+      "Nurturing a positive reputation by building trust to drive sustainable business growth.",
+    icon: "tabler:award",
+  },
+  {
+    title: "Email & SMS Marketing",
+    description:
+      "Enhance customer engagement, nurture relationships with direct communication",
+    icon: "mdi:email-seal-outline",
+  },
+  {
+    title: "Paid Advertising Campaigns",
+    description: "Fuel your growth with effective paid advertising Campaigns",
+    icon: "icons8:advertising",
+  },
+  {
+    title: 'Content Creation',
+    description: 'Harness the power of compelling and valuable content',
+    icon: 'tabler:award',
+  },
 ];
 
 export const BY_NUMBERS = [
   {
-    number: '12+',
-    title: 'Clients'
-  }, {
-    number: '8+',
-    title: 'Partners'
-  }, {
-    number: '20+',
-    title: 'Projects'
-  }, {
-    number: '5+',
-    title: 'Years'
+    number: "12+",
+    title: "Clients",
   },
-]
+  {
+    number: "8+",
+    title: "Partners",
+  },
+  {
+    number: "20+",
+    title: "Projects",
+  },
+  {
+    number: "5+",
+    title: "Years",
+  },
+];
 
 export const FAQ_LIST = [
   {
-    image_url: 'https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/illustrations/rocket-white.png',
-    question: 'Can you work within my limited marketing budget?',
-    answer: 'Absolutely! Working with small budgets is a common challenge, and I specialize in helping businesses make the most of their resources. I can identify cost-effective tactics, prioritize activities with the highest impact, and leverage affordable digital platforms to optimize your marketing within your financial constraints.'
+    image_url:
+      "https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/illustrations/rocket-white.png",
+    question: "Can you work within my limited marketing budget?",
+    answer:
+      "Absolutely! Working with small budgets is a common challenge, and I specialize in helping businesses make the most of their resources. I can identify cost-effective tactics, prioritize activities with the highest impact, and leverage affordable digital platforms to optimize your marketing within your financial constraints.",
   },
   {
-    image_url: 'https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/illustrations/settings.png',
-    question: 'How can hiring you be cost-effective for my business?',
-    answer: 'By leveraging my expertise and industry knowledge, I can help you make informed decisions, avoid costly mistakes, and allocate your budget wisely. This leads to optimized marketing strategies and a higher return on investment (ROI) for your marketing efforts.'
+    image_url:
+      "https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/illustrations/settings.png",
+    question: "How can hiring you be cost-effective for my business?",
+    answer:
+      "By leveraging my expertise and industry knowledge, I can help you make informed decisions, avoid costly mistakes, and allocate your budget wisely. This leads to optimized marketing strategies and a higher return on investment (ROI) for your marketing efforts.",
   },
   {
-    image_url: 'https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/illustrations/chat.png',
-    question: 'How do I know if my business needs the assistance of a marketer?',
-    answer: `If you're struggling to achieve your marketing goals, lack a clear strategy, or need fresh insights and ideas, it's a good indication that your business could benefit from my assistance as a marketer. I can assess your current efforts, identify areas for improvement, and develop a customized plan to drive growth and increase brand visibility.`
+    image_url:
+      "https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/illustrations/chat.png",
+    question:
+      "How do I know if my business needs the assistance of a marketer?",
+    answer: `If you're struggling to achieve your marketing goals, lack a clear strategy, or need fresh insights and ideas, it's a good indication that your business could benefit from my assistance as a marketer. I can assess your current efforts, identify areas for improvement, and develop a customized plan to drive growth and increase brand visibility.`,
   },
   {
-    image_url: 'https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/illustrations/folder.png',
-    question: 'How long does it usually take to see tangible results from marketing efforts?',
-    answer: 'The timeframe can vary depending on the industry, target audience, and chosen marketing channels. Some strategies may yield immediate results, while others require more time to gain traction. During our collaboration, I will set realistic expectations and develop a timeline based on your objectives and business circumstances.'
-  }
+    image_url:
+      "https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/illustrations/folder.png",
+    question:
+      "How long does it usually take to see tangible results from marketing efforts?",
+    answer:
+      "The timeframe can vary depending on the industry, target audience, and chosen marketing channels. Some strategies may yield immediate results, while others require more time to gain traction. During our collaboration, I will set realistic expectations and develop a timeline based on your objectives and business circumstances.",
+  },
   // , {
   //   question: 'Will you provide guidance on maximizing my marketing efforts while minimizing expenses?',
   //   answer: 'Absolutely! One of my primary objectives as a marketing consultant is to help businesses optimize their marketing efforts within their budget limitations. I can provide strategies to maximize your ROI, identify cost-effective marketing channels, and suggest innovative approaches to achieve your marketing goals while minimizing expenses.'
   // }
-]
+];
 
 export const HOW_IT_WORKS_STEP = [
   {
     title: "Initial Meeting",
     subtitle: "(30m - 1 hr)",
-    description: "Let’s discuss your needs and ensure the perfect match."
-  }, {
+    description: "Let’s discuss your needs and ensure the perfect match.",
+  },
+  {
     title: "Proposal and Quote",
-    description: "I will follow up with a proposal to start the FREE 30-days service."
-  }, {
+    description:
+      "I will follow up with a proposal to start the FREE 30-days service.",
+  },
+  {
     title: "Kick-Off Meeting",
     subtitle: "(1 - 2hrs)",
-    description: "Meeting to dive deeply into the project requirements, prioritize tasks, and set timelines and goals."
-  }, {
+    description:
+      "Meeting to dive deeply into the project requirements, prioritize tasks, and set timelines and goals.",
+  },
+  {
     title: "Measure it",
-    description: "We will track and measure the results of our efforts."
-  }
+    description: "We will track and measure the results of our efforts.",
+  },
 ];
 
 export const SITE = {
-  name: 'Vymktg',
-  title: 'Vy Trinh Marketer',
-  description: 'Professional Digital Marketer',
-  url: 'https://vymktg.github.io/vymktg.com',
-  githubUrl: 'https://github.com/vymktg/vymktg.com',
-  image: 'https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png',
+  name: "Vymktg",
+  title: "Vy Trinh Marketer",
+  description: "Professional Digital Marketer",
+  url: "https://vymktg.github.io/vymktg.com",
+  githubUrl: "https://github.com/vymktg/vymktg.com",
+  image:
+    "https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png",
   // YT video channel Id (used in media.astro)
-  ytChannelId: '',
+  ytChannelId: "",
   // Optional, user/author settings (example)
   // Author: name
-  author: '', // Example: Fred K. Schott
+  author: "", // Example: Fred K. Schott
   // Author: Twitter handler
-  authorTwitter: '', // Example: FredKSchott
+  authorTwitter: "", // Example: FredKSchott
   // Author: Image external source
-  authorImage: '', // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
+  authorImage: "", // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
   // Author: Bio
-  authorBio: 'Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro'
-
-}
-
+  authorBio:
+    "Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro",
+};
