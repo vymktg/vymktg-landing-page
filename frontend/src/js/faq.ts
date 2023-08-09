1260f8436abb8a0096da8cc666fddd3f17214540
datnguyen-nfq <dat.nguyen@nfq.com>
2023-08-09 22:45:25 +0700

    Adjust Footer and FAQ, add hover animation

--- frontend/src/js/faq.ts
+++ frontend/src/js/faq.ts
@@ -1,44 +1,49 @@
-
 const initialFAQ = () => {
-    const faqList = document.querySelectorAll<HTMLElement>("#faq .faq-item");
+  const faqList = document.querySelectorAll<HTMLElement>("#faq .faq-item");
 
-    faqList.forEach((item: HTMLElement) => {
-        item.addEventListener("click", () => {
-            onClickAccordion(item, faqList);
-        });
+  faqList.forEach((item: HTMLElement) => {
+    item.addEventListener("click", () => {
+      onClickAccordion(item, faqList);
     });
-}
-
-const onClickAccordion = (item: HTMLElement, faqList: NodeListOf<HTMLElement>) => {
-    if (!item.hasAttribute("open")) {
-        openAccordion(item);
-        closeAnotherAccordion(item, faqList);
-    } else {
-        closeAccordion(item);
-    }
+  });
 };
 
-const closeAnotherAccordion = (item: HTMLElement, faqList: NodeListOf<HTMLElement>) => {
-    faqList.forEach((element) => {
-        if (element.id !== item.id) {
-            closeAccordion(element);
-        }
-    });
-}
+const onClickAccordion = (
+  item: HTMLElement,
+  faqList: NodeListOf<HTMLElement>
+) => {
+  if (!item.hasAttribute("open")) {
+    openAccordion(item);
+    closeAnotherAccordion(item, faqList);
+  } else {
+    closeAccordion(item);
+  }
+};
+
+const closeAnotherAccordion = (
+  item: HTMLElement,
+  faqList: NodeListOf<HTMLElement>
+) => {
+  faqList.forEach((element) => {
+    if (element.id !== item.id) {
+      closeAccordion(element);
+    }
+  });
+};
 
 const openAccordion = (element: HTMLElement) => {
-    const answerElement = element.getElementsByClassName(
-        "answer"
-    )[0] as HTMLElement;
+  const answerElement = element.getElementsByClassName(
+    "answer"
+  )[0] as HTMLElement;
 
-    element.setAttribute("open", "true");
-    answerElement.style.maxHeight = `${answerElement.scrollHeight + 80}px`;
+  element.setAttribute("open", "true");
+  answerElement.style.maxHeight = `${answerElement.scrollHeight + 80}px`;
 };
 
 const closeAccordion = (element: HTMLElement) => {
-    const answerEl = element.getElementsByClassName("answer")[0] as HTMLElement;
-    element.removeAttribute("open");
-    answerEl.style.maxHeight = `0px`;
+  const answerEl = element.getElementsByClassName("answer")[0] as HTMLElement;
+  element.removeAttribute("open");
+  answerEl.style.maxHeight = `0px`;
 };
 
 initialFAQ();
