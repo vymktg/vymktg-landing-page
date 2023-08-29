fd94a003fa308c5fa64904b624d9fcefc0db9661
datnguyen-nfq <dat.nguyen@nfq.com>
2023-08-29 10:03:39 +0700

    Update active page condition

--- frontend/src/js/helper.ts
+++ frontend/src/js/helper.ts
@@ -1,5 +1,8 @@
 const isActivePage = (pathName: string, href: string) => {
-  const itemHref = import.meta.env.PROD ? `${href}/` : href;
+  const itemHref =
+    href !== import.meta.env.BASE_URL && import.meta.env.PROD
+      ? `${href}/`
+      : href;
 
   return pathName === itemHref;
 };
