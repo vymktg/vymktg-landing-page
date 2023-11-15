b3ae2d559fffbc5354be8b60cd12a24db831ace1
datnguyen-nfq <dat.nguyen@nfq.com>
2023-11-16 00:16:13 +0700

    Deploy hubspot form

--- frontend/src/js/index-init.ts
+++ frontend/src/js/index-init.ts
@@ -1,3 +1,47 @@
 import "./aos";
 import "./faq";
 import "./how-it-works";
+
+declare global {
+  interface Window {
+    hsConversationsSettings: Object;
+  }
+  var HubSpotConversations: {
+    widget: {
+      remove: () => void;
+      load: () => void;
+    };
+  };
+}
+
+window.onload = () => {
+  window.hsConversationsSettings = {
+    loadImmediately: false,
+    inlineEmbedSelector: "#hubspot-conversations-inline-parent",
+    enableWidgetCookieBanner: true,
+    disableAttachment: true,
+  };
+
+  window.hsConversationsOnReady = [
+    () => {
+      window.HubSpotConversations.widget.load();
+    },
+  ];
+};
+
+function onConversationsAPIReady() {
+  console.log(`HubSpot Conversations API: ${window.HubSpotConversations}`);
+}
+
+/*
+   If external API methods are already available, use them.
+  */
+if (window.HubSpotConversations) {
+  onConversationsAPIReady();
+} else {
+  /*
+      Otherwise, callbacks can be added to the hsConversationsOnReady on the window object.
+      These callbacks will be called once the external API has been initialized.
+    */
+  window.hsConversationsOnReady = [onConversationsAPIReady];
+}
