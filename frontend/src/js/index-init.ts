b87274760458ed7327fe584828f73e9404950973
datnguyen-nfq <dat.nguyen@nfq.com>
2023-11-16 22:31:38 +0700

    Revert hubspot

--- frontend/src/js/index-init.ts
+++ frontend/src/js/index-init.ts
@@ -15,22 +15,35 @@ declare global {
   };
 }
 
-const onConversationsAPIReady = () => {
-  console.log(`HubSpot Conversations API: ${window.HubSpotConversations}`);
-  window.HubSpotConversations.widget.load({ widgetOpen: true});
-}
-/*
-  configure window.hsConversationsSettings if needed.
-*/
-window.hsConversationsSettings = {
-  loadImmediately: true,
-  inlineEmbedSelector: "#hubspot-conversations-inline-parent",
+
+window.onload = () => {
+  window.hsConversationsSettings = {
+    loadImmediately: false,
+    inlineEmbedSelector: "#hubspot-conversations-inline-parent",
+    enableWidgetCookieBanner: true,
+    disableAttachment: true,
+  };
+
+  window.hsConversationsOnReady = [
+    () => {
+      window.HubSpotConversations.widget.load({});
+    },
+  ];
+
+  function onConversationsAPIReady() {
+    console.log(`HubSpot Conversations API: ${window.HubSpotConversations}`);
+  }
+
+  /*
+   If external API methods are already available, use them.
+  */
+  if (window.HubSpotConversations) {
+    onConversationsAPIReady();
+  } else {
+    /*
+      Otherwise, callbacks can be added to the hsConversationsOnReady on the window object.
+      These callbacks will be called once the external API has been initialized.
+    */
+    window.hsConversationsOnReady = [onConversationsAPIReady];
+  }
 };
-/*
- If external API methods are already available, use them.
-*/
-if (window.HubSpotConversations) {
-  console.log('The api is ready already');
-} else {
-  window.hsConversationsOnReady = [onConversationsAPIReady];
-}
