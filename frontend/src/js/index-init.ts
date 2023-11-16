54efdacdcd34e24ad7dd33879b2e5c04be665dd7
datnguyen-nfq <dat.nguyen@nfq.com>
2023-11-16 22:59:30 +0700

    Adjust hubspot page

--- frontend/src/js/index-init.ts
+++ frontend/src/js/index-init.ts
@@ -1,3 +1,31 @@
 import "./aos";
 import "./faq";
 import "./how-it-works";
+
+declare global {
+  interface Window {
+    HubSpotConversations: any;
+    hsConversationsSettings: object;
+    hsConversationsOnReady: object;
+  }
+}
+
+function onConversationsAPIReady() {
+  console.log(`HubSpot Conversations API: ${window.HubSpotConversations}`);
+}
+/*
+  configure window.hsConversationsSettings if needed.
+*/
+window.hsConversationsSettings = {};
+/*
+ If external API methods are already available, use them.
+*/
+if (window.HubSpotConversations) {
+  onConversationsAPIReady();
+} else {
+  /*
+    Otherwise, callbacks can be added to the hsConversationsOnReady on the window object.
+    These callbacks will be called once the external API has been initialized.
+  */
+  window.hsConversationsOnReady = [onConversationsAPIReady];
+}
