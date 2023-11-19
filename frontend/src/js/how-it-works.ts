9fbed07c86276400d31172a885171de41f469667
datnguyen-nfq <dat.nguyen@nfq.com>
2023-11-19 21:46:01 +0700

    Adjust fast timer

--- frontend/src/js/how-it-works.ts
+++ frontend/src/js/how-it-works.ts
@@ -17,7 +17,7 @@ svgLayerElement.forEach((element: Element) => {
 });
 
 const svgLength = svgElement.getTotalLength();
-const { fastTimer = 8, isReverse = false } = svgElement.dataset;
+const { fastTimer = 10, isReverse = false } = svgElement.dataset;
 
 svgElement.style.strokeDasharray = `${svgLength}`;
 svgElement.style.strokeDashoffset = `${svgLength}`;
