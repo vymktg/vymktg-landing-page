f6c693c5054e284f8ecc348e7bebac9ebe697004
datnguyen-nfq <dat.nguyen@nfq.com>
2024-02-21 10:17:55 +0700

    Adjust new template

--- frontend/src/components/sections/HowItWork/step-slider.ts
+++ frontend/src/components/sections/HowItWork/step-slider.ts
@@ -8,7 +8,7 @@ const config = {
       perPage: 1,
     },
   },
-  arrows: false,
+  arrows: true,
   classes: {
     pagination: "splide__pagination !-bottom-5 splide__pagination--custom",
   }, 
@@ -20,16 +20,16 @@ const slidePhases = document.querySelectorAll(".splide__phase");
 slidePhases.forEach((slide) => {
   const slideEl = new Splide(slide, config);
 
-//   slideEl.on("pagination:mounted", function (data) {
-//     // You can add your class to the UL element
-//     data.list.classList.add("splide__pagination--custom");
+  // slideEl.on("pagination:mounted", function (data) {
+  //   // You can add your class to the UL element
+  //   data.list.classList.add("splide__pagination--custom");
 
-//     // `items` contains all dot items
-//     data.items.forEach(function (item) {
-//       //item.button.textContent = String(item.page + 1);
-//       //item.button.classList.add("");
-//     });
-//   });
+  //   // `items` contains all dot items
+  //   data.items.forEach(function (item) {
+  //     item.button.textContent = String(item.page + 1);
+  //     //item.button.classList.add("");
+  //   });
+  // });
 
   slideEl.mount();
 });
